29ec34177e75285cfcea8266f7bdca43c5016b3a
Klara Modin <klarasmodin@gmail.com>
2025-11-04 21:36:24 +0100

    wip: break recursion

--- src/SKGraphSchema.hs
+++ src/SKGraphSchema.hs
@@ -13,8 +13,9 @@ data SModelElement = SModelElement
 
 instance HasCodec SModelElement where
   codec =
-    object "SModelElement" $
-      SModelElement
-        <$> requiredField "type" "element type" .= sType
-        <*> requiredField "id" "element id" .= sId
-        <*> requiredField "children" "element children" .= sChildren
+    named "SModelElementCodec" $
+      object "SModelElement" $
+        SModelElement
+          <$> requiredField "type" "element type" .= sType
+          <*> requiredField "id" "element id" .= sId
+          <*> optionalFieldWithDefault "children" [] "children of the element" .= sChildren
